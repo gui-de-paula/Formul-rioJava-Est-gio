76e70bdb96defc72de2670f2e506fe345e6281fd
Guilherme de Paula <guilherme.teixeira@docnix.com.br>
2022-09-14 11:06:11 -0300

    Começando projeto em Java

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Menu {
+    public static void main(String [] args){
+        Scanner scanner= new Scanner(System.in);
+
+    }
+
+}
